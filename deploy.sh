554792279b4684bb14c3d8e9bf5d90db5338fa5c
tanaseana253 <tanase_ana253@yahoo.com>
2025-10-24 10:17:43 +0300

    deploy

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd image-search-frontend
-ng build
-cd ..
-rm -rf image-search-backend/static/* && cp -r image-search-frontend/dist/image-search-frontend/* image-search-backend/static/
-git add .
-git commit -m "Deploy"
-git push
